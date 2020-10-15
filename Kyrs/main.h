3827d6bf6c96652f11599be02820bdba7d728ef3
Alexey Timofievich <alex.timofievich@gmail.com>
2020-10-15 13:50:51 +0300

    Add animation

---
+++ Kyrs/main.h
@@ -0,0 +1,17 @@
+//
+// Created by Алексей on 15.10.2020.
+//
+
+#ifndef KYRS_MAIN_H
+#define KYRS_MAIN_H
+
+const int GAME_CIRCLE_RADIUS = 200;
+const int ATOM_DIAMETER = 40;
+const float PI = 3.14;
+
+const int WINDOW_WIDTH = 600;
+const int WINDOW_HEIGHT = 800;
+const COLORREF BACKGROUND = RGB(139, 0, 0);
+const int COLOR_SHIFT = 30;
+
+#endif //KYRS_MAIN_H
